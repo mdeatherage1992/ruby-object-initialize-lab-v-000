7714da6797c6ec0644d7fb6ca7ad2ce0a8b5bc32
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-06-12 12:49:43 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,21 +1,7 @@
 class Dog
-  def initialize(name)
+  def initialize(name,breed)
     @name = name
-  end
-  def name=(name)
-    @name = Fido
+    @breed = breed
   end
   def name
-    @name
-  end
-def initialize(breed)
-    @breed = breed
-  end
-
-  def breed=(breed)
-    @breed = breed
-  end
-  def breed
-    @breed
-  end
-end
+  
