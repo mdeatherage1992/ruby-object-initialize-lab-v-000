222ad7d26d1320bc82048d18d0ff74ab0bfc15b3
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-06-12 12:14:25 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,7 +3,7 @@ class Dog
     @name = name
   end
   def name=(name)
-    @name = name
+    @name = Fido
   end
   def name
     @name
@@ -12,7 +12,7 @@ class Dog
     @breed = breed
   end
   def breed=(breed)
-    @breed = breed
+    @breed = Mutt
   end
   def breed
     @breed
